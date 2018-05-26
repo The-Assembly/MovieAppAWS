45a285cb07845770602bca0af3289efd6235db56
The Assembly <nikhil@theassembly.ae>
2018-05-26 12:14:56 +0400

    Initial

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	API_KEY = "921303bd9d6beed719ae88231d2534db"
+)
+
+//go get github.com/aws/aws-lambda-go/lambda
+//go get github.com/stretchr/testify
+
+func main() {
+
+}
